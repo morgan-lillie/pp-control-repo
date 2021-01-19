b1dc8c9df81000a775a0b85e1f1404b5cdcec72e
SE Demo User <morgan.lillie@puppet.com>
2021-01-19 15:17:16 +0000

    Did Labs 4.2 and 4.3

---
+++ modules/review/manifests/motd.pp
@@ -0,0 +1,16 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include review::motd
+class review::motd {
+
+  file { '/etc/motd':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => epp('review/motd.epp', { fqdn => $facts['networking']['fqdn']}),
+  }
+}
